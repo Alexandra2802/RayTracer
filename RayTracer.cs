a177cae95f495a5ae48f2240ca7b4ac0175f5052
Alexandra Balo <alexandrabalo@yahoo.com>
2024-02-13 15:10:28 +0200

    initial commit

---
+++ RayTracer.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace rt
+{
+    class RayTracer
+    {
+        private Geometry[] geometries;
+        private Light[] lights;
+
+        public RayTracer(Geometry[] geometries, Light[] lights)
+        {
+            this.geometries = geometries;
+            this.lights = lights;
+        }
+
+        private double ImageToViewPlane(int n, int imgSize, double viewPlaneSize)
+        {
+            var u = n * viewPlaneSize / imgSize;
+            u -= viewPlaneSize / 2;
+            return u;
+        }
+
+        private Intersection FindFirstIntersection(Line ray, double minDist, double maxDist)
+        {
+            var intersection = new Intersection();
+
+            foreach (var geometry in geometries)
+            {
+                var intr = geometry.GetIntersection(ray, minDist, maxDist);
+
+                if (!intr.Valid || !intr.Visible) continue;
+
+                if (!intersection.Valid || !intersection.Visible)
+                {
+                    intersection = intr;
+                }
+                else if (intr.T < intersection.T)
+                {
+                    intersection = intr;
+                }
+            }
+
+            return intersection;
+        }
+
+        private bool IsLit(Vector point, Light light)
+        {
+            // ADD CODE HERE: Detect whether the given point has a clear line of sight to the given light
+            Vector pointToLight = light.Position - point;
+            Intersection intersection = FindFirstIntersection(new Line(point, light.Position), 0, pointToLight.Length());
+            return !intersection.Visible || !intersection.Valid;
+        }
+
+        public void Render(Camera camera, int width, int height, string filename)
+        {
+            var background = new Color();
+            var image = new Image(width, height);
+
+            for (var i = 0; i < width; i++)
+            {
+                for (var j = 0; j < height; j++)
+                {
+                    // ADD CODE HERE: Implement pixel color calculation
+                    var x1 = camera.Position +
+                            camera.Direction * camera.ViewPlaneDistance +
+                            camera.Up * ImageToViewPlane(j, height, camera.ViewPlaneHeight) +
+                            (camera.Up^camera.Direction) * ImageToViewPlane(i, width, camera.ViewPlaneWidth);
+                    Line ray = new Line(camera.Position, x1);
+                    Intersection intersection = FindFirstIntersection(ray, camera.FrontPlaneDistance, camera.BackPlaneDistance);
+                    Geometry geometry = intersection.Geometry;
+
+                    if (intersection.Valid)
+                    {
+                        Vector N = intersection.Geometry.Normal(intersection.Position);
+                        Vector E = (camera.Position - intersection.Position).Normalize();
+                        Color color = geometry.Material.Ambient;
+
+                        foreach (Light light in lights)
+                        {
+                            color *= light.Ambient;
+                            if (IsLit(intersection.Position, light))
+                            {
+                                Vector T = (light.Position - intersection.Position).Normalize();
+                                Vector R = (N * (N * T) * 2 - T).Normalize();
+                                if (N * T > 0)
+                                {
+                                    color += geometry.Material.Diffuse * light.Diffuse * (N * T);
+                                }
+                                if (E * R > 0)
+                                {
+                                    color += geometry.Material.Specular * light.Specular * Math.Pow(E * R, geometry.Material.Shininess);
+                                }
+                                color *= light.Intensity;
+                            }
+                        }
+                        image.SetPixel(i, j, color);
+                    }
+                }
+            }
+            image.Store(filename);
+        }
+    }
+}
---
+++ Sphere.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace rt
+{
+    public class Sphere : Geometry
+    {
+        private Vector Center { get; set; }
+        private double Radius { get; set; }
+
+        public Sphere(Vector center, double radius, Material material, Color color) : base(material, color)
+        {
+            Center = center;
+            Radius = radius;
+        }
+
+        public override Intersection GetIntersection(Line line, double minDist, double maxDist)
+        {
+            // ADD CODE HERE: Calculate the intersection between the given line and this sphere
+            bool valid;
+            bool visible;
+            double t;
+
+            var a = line.Dx.X;
+            var c = line.Dx.Y;
+            var e = line.Dx.Z;
+
+            var b = line.X0.X;
+            var d = line.X0.Y;
+            var f = line.X0.Z;
+
+            var A = a * a + c * c + e * e;
+            var B = 2 * (a * b - a * Center.X + c * d - c * Center.Y + e * f - e * Center.Z);
+            var C = b * b + Center.X * Center.X - 2 * b * Center.X + d * d + Center.Y * Center.Y - 2 * d * Center.Y + f * f + Center.Z * Center.Z - 2 * f * Center.Z - Radius * Radius;
+            //A*t^2+B*t+C=0
+            var delta = B * B - 4 * A * C;
+            valid = (delta >= 0);
+            if (valid)
+            {
+                var t1 = (-B + Math.Sqrt(delta)) / 2 * A;
+                var t2 = (-B - Math.Sqrt(delta)) / 2 * A;
+                t = t1 < t2 ? t1 : t2;
+            }
+            else
+                t = -1;
+            visible=(minDist<t && t<maxDist);
+
+            return new Intersection(valid, visible, this, line, t);
+        }
+
+        public override Vector Normal(Vector v)
+        {
+            var n = v - Center;
+            n.Normalize();
+            return n;
+        }
+    }
+}
